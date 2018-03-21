683dee1eff0265bfc4a72d5ce5581599e48ce940
Jason Salerno <jasons@jellyvision.com>
2018-03-21 12:48:58 -0500

    add reactstrap and navigation bar wip

---
+++ app/javascript/components/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import NavigationBar from './navigation-bar';
+
+const Main = props => (
+  <React.Fragment>
+    <NavigationBar />
+  </ React.Fragment>
+);
+
+export default Main;
