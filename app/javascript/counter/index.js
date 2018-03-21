24a3e98ffb56034d761b202cda933103bf215a60
Jason Salerno <jasons@jellyvision.com>
2018-03-21 11:14:11 -0500

    add simple js counter example of using with webpacker

---
+++ app/javascript/counter/index.js
@@ -0,0 +1,7 @@
+// Initialize the counter code when DOM is ready
+import './style.scss';
+import counter from './counter';
+
+document.addEventListener('DOMContentLoaded', () => {
+  counter.initialize();
+});
