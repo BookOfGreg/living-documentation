e7a256abf1f89a79d9c11b5121a6d9393667eec8
Jason Salerno <jasons@jellyvision.com>
2018-03-23 17:27:15 -0500

    updated links to use more dropdown

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,9 +16,15 @@ class ApplicationController < ActionController::Base
         { title: 'View Categories', path: categories_path },
       ]
 
-      @enabled_links += [
-        { title: 'Add Post', path: new_post_path },
-        { title: 'Add Category', path: new_category_path },
+      @enabled_links = [
+        { title: 'Posts', dropdown: true, links: [
+          @enabled_links[0],
+          { title: 'Add Post', path: new_post_path }
+        ]},
+        { title: 'Categories', dropdown: true, path: categories_path, links: [
+          @enabled_links[1],
+          { title: 'Add Category', path: new_category_path }
+        ]},
         { title: 'Profile', dropdown: true, links: [
           { title: 'Edit Profile', path: edit_user_registration_path },
           { title: 'Logout', path: destroy_user_session_path, attributes: { rel: 'nofollow', 'data-method': 'delete' }}
