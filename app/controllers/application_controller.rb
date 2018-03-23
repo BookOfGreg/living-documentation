f6d66a5ffc914a48637ea2b4d3c6269848f4d598
Jason Salerno <jasons@jellyvision.com>
2018-03-22 22:46:08 -0500

    updated application and navigation bar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,25 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :enable_links
 
   protected
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-  end
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    end
+
+    def enable_links
+      @enabled_links = [
+        { title: 'View Posts', path: posts_path },
+        { title: 'View Categories', path: categories_path },
+      ]
+
+      @enabled_links += [
+        { title: 'Add Posts', path: new_post_path },
+        { title: 'Add Category', path: new_user_session_path },
+        { title: 'Logout', path: destroy_user_session_path, delete: true }
+      ] if user_signed_in?
+    end
 end
--- app/javascript/components/NavigationBar.jsx
+++ app/javascript/components/NavigationBar.jsx
@@ -19,18 +19,29 @@ class NavigationBar extends Component {
   constructor(props) {
     super(props);
 
-    this.toggle = this.toggle.bind(this);
     this.state = {
       isOpen: false
     };
   }
-  toggle() {
+
+  toggle = () => {
     this.setState({
       isOpen: !this.state.isOpen
     });
   }
+
+  createNavItems(links) {
+    return links.map(link => {
+      return (
+        <NavItem key={link.title}>
+          <NavLink href={link.path}>{link.title}</NavLink>
+        </NavItem>
+      );
+    });
+  }
+
   render() {
-    const { navbarBrand } = this.props;
+    const { navbarBrand, enabledLinks } = this.props;
 
     return (
       <div>
@@ -39,29 +50,7 @@ class NavigationBar extends Component {
           <NavbarToggler onClick={this.toggle} />
           <Collapse isOpen={this.state.isOpen} navbar>
             <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink href="/components/">Components</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="https://github.com/reactstrap/reactstrap">Github</NavLink>
-              </NavItem>
-              <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle nav caret>
-                  Options
-                </DropdownToggle>
-                <DropdownMenu >
-                  <DropdownItem>
-                    Option 1
-                  </DropdownItem>
-                  <DropdownItem>
-                    Option 2
-                  </DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem>
-                    Reset
-                  </DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
+              {this.createNavItems(enabledLinks)}
             </Nav>
           </Collapse>
         </Navbar>
