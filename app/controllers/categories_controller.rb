206a9097d98610f5138f719b7b9dfff3445657e9
Jason Salerno <jasons@jellyvision.com>
2018-03-24 14:54:22 -0500

    updated to enable categories to use Listing.jsx

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,11 +2,13 @@ class CategoriesController < ApplicationController
   before_action :set_category, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
   before_action :correct_user, only: [:edit, :update, :destory]
+  before_action :set_listing
 
   # GET /categories
   # GET /categories.json
   def index
     @categories = Category.all
+    @categories_list = CategoriesListService.new(@categories, current_user).list
   end
 
   # GET /categories/1
@@ -77,5 +79,15 @@ class CategoriesController < ApplicationController
     def correct_user
       @category = current_user.categories.find_by(id: params[:id])
       redirect_to categories_path, notice: 'Not authorized to edit this category' if @category.nil?
+    end
+
+    def set_listing
+      @categories_list = {
+        headings: [
+          { name: 'Name' },
+          { attributes: { colSpan: 3 } }
+        ],
+        rows: @posts
+      }
     end
 end
---
+++ app/services/categories_list_service.rb
@@ -0,0 +1,55 @@
+class CategoriesListService
+  def initialize(categories, current_user)
+    @current_user = current_user
+    @categories = categories
+  end
+
+  def list
+    {
+      headings: build_headings,
+      keys: build_keys,
+      rows: build_list,
+      raw: @categories
+    }
+  end
+
+  private
+
+    def build_keys
+      keys = [ 'name', 'show' ]
+      keys += ['edit', 'delete'] if @current_user
+      keys
+    end
+
+    def build_headings
+      headings = [
+        { name: 'Name' },
+        { attribute: { colSpan: '1' } }
+      ]
+      headings += [{ attributes: { colSpan: '2' } }] if @current_user
+      headings
+    end
+
+    def check_admin_access(category)
+      return {} unless @current_user
+      return {} unless @current_user.id == category.user_id
+      {
+        edit: { text: 'Edit', link: true, attributes: { href: "/categories/#{category.id}/edit" } },
+        delete: { text: 'Destroy', link: true, attributes: {
+          href: "/categories/#{category.id}",
+          rel: 'nofollow',
+          'data-method': 'delete',
+          'data-confirm': 'Are you sure?'
+        } },
+      }
+    end
+
+    def build_list
+      @categories.map{ |category|
+        {
+          name: { text: category.name },
+          show: { text: 'Show', link: true, attributes: { href: "/categories/#{category.id}" }  }
+        }.merge(check_admin_access(category))
+      }
+    end
+end
